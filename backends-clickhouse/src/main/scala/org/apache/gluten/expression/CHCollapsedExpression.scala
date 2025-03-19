1ef8a02578f87e566eeda0039889734b830bffe9
BIGO <bigo@BIGOdeMacBook-Pro.local>
2025-03-19 19:17:16 +0800

    1111

--- backends-clickhouse/src/main/scala/org/apache/gluten/expression/CHCollapsedExpression.scala
+++ backends-clickhouse/src/main/scala/org/apache/gluten/expression/CHCollapsedExpression.scala
@@ -23,8 +23,11 @@ import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
 import org.apache.spark.sql.types.DataType
 
-abstract class CHCollapsedExpression(children: Seq[Expression] = Seq.empty, name: String = "")
-  extends Expression {
+abstract class CHCollapsedExpression(children: Seq[Expression], name: String) extends Expression {
+
+  def this() {
+    this(Seq.empty[Expression], "")
+  }
 
   override def toString: String = s"$name(${children.mkString(", ")})"
 
